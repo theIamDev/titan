7d2f95d0c2e1734c9ff6a944ed483e97ed1ea2f0
iamdev <theiamdev@gmail.com>
2026-06-13 00:44:33 +0530

    new project changes for local

--- titan/settings.py
+++ titan/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'titan.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'vmss$vms_db',
+		'USER':'vmss',
+		'PASSWORD' : 'Car@2fast',
+		'HOST':'vmss.mysql.pythonanywhere-services.com',
+		'PORT':'3306',
     }
 }
 
